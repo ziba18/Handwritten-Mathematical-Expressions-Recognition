53ae7576e564058c1f4a7c9d20fa7c354682333c
Mo Sorkhpar <Sorkhpar@outlook.com>
2023-05-28 00:32:53 -0400

    Skeletonize input image
    - setup input and output directories and paths
    - modify configuration properties
    - use OpenCV and skimage to extract the skeleton of the input image

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 import logging
-import os
 
-from dotenv import load_dotenv
-
+from processors import image_processor
 from utils.config import Config
 
 if __name__ == '__main__':
-    load_dotenv('.env')
     config = Config()
-
     logging.basicConfig(format='%(asctime)s %(levelname)s:%(name)s:%(funcName)s:%(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S',
                         level=config.log_level)
     logger = logging.getLogger(__name__)
-
     logger.info(f"Loaded configurations:{config}")
+
+    skeleton = image_processor.process_image()
